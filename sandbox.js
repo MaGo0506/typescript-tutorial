b6c3c4fc0c4b041be6fe2a396f4da5a09a9499dc
Marko Goronjic <marko.goronjic@quantox.com>
2023-01-06 13:26:44 +0100

    Type Basics

---
+++ sandbox.js
@@ -0,0 +1,13 @@
+var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+// character = 40;
+character = 'luigi';
+// age = 'yoshi';
+age = 30;
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(2));
---
+++ sandbox.ts
@@ -0,0 +1,18 @@
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
+
+// character = 40;
+character = 'luigi';
+
+// age = 'yoshi';
+age = 30;
+
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI
+}
+
+console.log(circ(2))
